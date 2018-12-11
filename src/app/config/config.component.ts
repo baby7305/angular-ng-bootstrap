56e965dd692467914ca8a96237b36ab6ff149a51
baby7305 <songhaibei7305@163.com>
2018-12-11 18:31:56 +0800

    alert demo 配置

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
-  styleUrls: ['./config.component.scss']
+  styleUrls: ['./config.component.scss'],
+  // add NgbAlertConfig  to the component providers
+  providers: [NgbAlertConfig]
 })
 export class ConfigComponent implements OnInit {
 
-  constructor() { }
+  @Input() public alerts: Array<string> = [];
+
+  constructor(alertConfig: NgbAlertConfig) {
+    // customize default values of alerts used by this component tree
+    alertConfig.type = 'success';
+    alertConfig.dismissible = false;
+  }
 
   ngOnInit() {
   }
--- src/app/selfclosing/selfclosing.component.ts
+++ src/app/selfclosing/selfclosing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-selfclosing',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelfclosingComponent implements OnInit {
 
+  private _success = new Subject<string>();
+
+  staticAlertClosed = false;
+  successMessage: string;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
+  }
+
+  public changeSuccessMessage() {
+    this._success.next(`${new Date()} - Message successfully changed.`);
   }
 
 }
